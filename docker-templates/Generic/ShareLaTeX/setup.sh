0f797514fe9d4d7bfa4cb85e84a972e0c1276de7
Pavan Gudimetta <pgudimetta@gmail.com>
2015-12-28 23:46:53 -0500

    added compatibility for setup script

--- docker-templates/Generic/ShareLaTeX/setup.sh
+++ docker-templates/Generic/ShareLaTeX/setup.sh
@@ -8,7 +8,9 @@
 #   DESCRIPTION:  Setup script for ShareLaTeX Docker Server
 # 
 #       OPTIONS:  ---
-#  REQUIREMENTS:  ---
+#  REQUIREMENTS:  This script can be used for both Redhat and Debian systems
+#									See comments for more details
+#
 #          BUGS:  ---
 #         NOTES:  ---
 #        AUTHOR:   (), 
@@ -18,12 +20,17 @@
 #      REVISION:  ---
 #===============================================================================
 docker_ip=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' $(docker ps | awk -v i=2
--v j=1 'FNR == i {print $j}')
-#checks if docker is installed 
+-v j=1 'FNR == i {print $j}'))
+
+#checks if docker is installed Debian 
 if [ $(dpkg-query -W -f='${Status}' docker 2>/dev/null | grep -c "ok installed") -eq 0 ];
 then
 	  apt-get install docker;
 	fi
+#If Redhat, comment above and then uncomment below
+# rpm -qa | grep -qw docker-engine || curl -sSL https://get.docker.com/ | sh
+
+
 # starts docker service 
 sudo service docker start;
 #builds ShareLaTeX images
@@ -40,9 +47,11 @@ docker run -d \
 #INSTALL ALL THE PACKAGES~!!!(-(0-0)-)
 docker exec sharelatex tlmgr install scheme-full;
 
-#insert
+#insert docker containers ip address into relevants configuration files (assumes 
+# only one container is running). Note, if multiple containers are running then
+# might have to do this manually
 sudo sed -i "i/63s/.*/bind $IP /g" /etc/redis.conf 
-sudo sed -i "i/6s/.*/bind $IP /g" /etc/mongod.conf 
+sudo sed -i "i/6s/.*/bind_ip $IP /g" /etc/mongod.conf 
 #shutdowns sharelatex server
 docker stop sharelatex;
 #replies that images is ready to run and then runs docker container
