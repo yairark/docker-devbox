3119b702d28fc5b5c781122c58d4e3290c620cd2
Pavan Gudimetta <pgudimetta@gmail.com>
2015-12-28 23:16:53 -0500

    finished setup script for sharelatex server

--- docker-templates/Generic/ShareLaTeX/setup.sh
+++ docker-templates/Generic/ShareLaTeX/setup.sh
@@ -17,23 +17,40 @@
 #       CREATED:  12/24/2015 12:31:34 PM EST
 #      REVISION:  ---
 #===============================================================================
-
+docker_ip=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' $(docker ps | awk -v i=2
+-v j=1 'FNR == i {print $j}')
 #checks if docker is installed 
-
-#if not,then installs docker
-
+if [ $(dpkg-query -W -f='${Status}' docker 2>/dev/null | grep -c "ok installed") -eq 0 ];
+then
+	  apt-get install docker;
+	fi
+# starts docker service 
+sudo service docker start;
 #builds ShareLaTeX images
+docker build -t "pggsx:sharelatex" .;
+
+#setups docker command for sharelatex server 
+docker run -d \
+	  -v ~/sharelatex_data:/var/lib/sharelatex \
+		  -p 5000:80 \
+			  --name=sharelatex \
+				sharelatex/sharelatex
 
 #runs docker exec for container to do full Tex-install
 #INSTALL ALL THE PACKAGES~!!!(-(0-0)-)
+docker exec sharelatex tlmgr install scheme-full;
 
-#setups docker command for sharelatex server
-
+#insert
+sudo sed -i "i/63s/.*/bind $IP /g" /etc/redis.conf 
+sudo sed -i "i/6s/.*/bind $IP /g" /etc/mongod.conf 
+#shutdowns sharelatex server
+docker stop sharelatex;
 #replies that images is ready to run and then runs docker container
+echo "Image was built successfully"
 sudo bash -c "sudo docker start sharelatex && sudo service mongod start && sudo service redis
 start"
 
 #echoes that container was built successfully and then exits
-echo "Build Successful"
+echo "Container is now running~! Access via localhost:5000"
 exit
 
